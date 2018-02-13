12b309082088eaba6d68b6248093d3cc51da6ca2
mazuh <marcell-mz@hotmail.com>
2018-02-13 15:39:48 -0300

    Check basics installation

---
+++ utils/check_basics_installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASICS=('git' 'vim' 'tmux' 'firefox' 'code' 'spotify')
+
+if [ `which apt-get` ]; then
+	echo "'apt-get' found! Trying to update sources list."
+	apt-get update
+	basics_installer_cmd="apt-get install --yes"
+	echo "'$basics_installer_cmd' selected for installing basics."
+else
+	echo "No supported package manager found."
+	exit 1
+fi
+
+for basic_pkg in "${BASICS[@]}"; do
+	if [ ! `which $basic_pkg` ]; then
+		echo -n "'$basic_pkg' was missing, trying to install... "
+		if [[ `$basics_installer_cmd $basic_pkg &> /dev/null` ]]; then
+			echo "Done."
+		else
+			echo "Failed!!!"
+		fi
+	else
+		echo "'$basic_pkg' already installed."
+	fi
+done
+
