b766e55a6b19f4ab556865ddc7a59cefcd0ffe9d
mazuh <marcell-mz@hotmail.com>
2018-02-14 22:42:38 -0300

    Add more to basics list and check for pkg too instead of just bin

--- utils/check_basics_installation.sh
+++ utils/check_basics_installation.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-BASICS=('git' 'vim' 'tmux' 'firefox' 'code' 'spotify' 'curl' 'python2' 'python3')
+BASICS=('tmux' 'vim' 'git' 'mercurial' 'meld' 'firefox' 'code'\
+	'spotify' 'curl' 'python2' 'python3' 'python-pip' 'python3-pip')
 
 if [ "$EUID" != 0 ]; then
 	echo "You need super user permission to run this script."
 	exit 1
 fi
 
+BIN_CHECKER="which"
+
 if [ `which apt-get` ]; then
-	echo "'apt-get' found! Trying to update sources list."
-	apt-get update
+	echo -n "'apt-get' found. Trying to update sources list (may take a while)... "
+	apt-get update --yes &> /dev/null
+	update_err=$?
+	if [ update_err = 0 ]; then
+		echo "Updated."
+	else
+		echo "Tried."
+	fi
 	basics_installer_cmd="apt-get install --yes"
+	pkg_checker="dpkg --status"
 	echo "'$basics_installer_cmd' selected for installing basics."
 else
 	echo "No supported package manager found."
 	exit 1
 fi
 
-for basic_pkg in "${BASICS[@]}"; do
-	if [ ! `which $basic_pkg` ]; then
-		echo -n "'$basic_pkg' was missing, trying to install... "
-		if [[ `$basics_installer_cmd $basic_pkg &> /dev/null` ]]; then
-			echo "Done."
-		else
-			echo "Failed!!!"
-		fi
+for pkg in "${BASICS[@]}"; do
+	$pkg_checker $pkg &> /dev/null
+	pkg_checker_err=$?
+	if [ `$BIN_CHECKER $pkg` ] || [ $pkg_checker_err = 0 ]; then
+		echo "'$pkg' already installed."
 	else
-		echo "'$basic_pkg' already installed."
+		echo -n "'$pkg' was missing, trying to install (may take a while)... "
+		$basics_installer_cmd "$pkg" &> /dev/null
+		pkg_installation_err=$?
+		if [ $pkg_installation_err = 0 ]; then
+			echo "Done, package installed."
+		else
+			echo "Failed at package manager!!!"
+		fi
 	fi
 done
 
